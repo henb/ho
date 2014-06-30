734fc20a99b21ac3c3adbeba26ea112fdf1cc7de
henb <henb2015@gmail.com>
2014-06-30 11:39:49 +0300

    revie

--- spec/factories/hotels.rb
+++ spec/factories/hotels.rb
@@ -1,12 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+# # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :hotel do
-    title "MyString"
-    breakfast_included false
-    room_description "MyText"
-    photo "MyString"
-    price_for_room 1
-    address "MyString"
-  end
-end
+# FactoryGirl.define do
+#   factory :hotel do
+#     title "MyString"
+#     breakfast_included false
+#     room_description "MyText"
+#     photo "MyString"
+#     price_for_room 1
+#     address "MyString"
+#   end
+# end
--- spec/views/hotels/edit.html.haml_spec.rb
+++ spec/views/hotels/edit.html.haml_spec.rb
@@ -1,28 +1,28 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "hotels/edit" do
-  before(:each) do
-    @hotel = assign(:hotel, stub_model(Hotel,
-      :title => "MyString",
-      :breakfast_included => false,
-      :room_description => "MyText",
-      :photo => "MyString",
-      :price_for_room => 1,
-      :address => "MyString"
-    ))
-  end
+# describe "hotels/edit" do
+#   before(:each) do
+#     @hotel = assign(:hotel, stub_model(Hotel,
+#       :title => "MyString",
+#       :breakfast_included => false,
+#       :room_description => "MyText",
+#       :photo => "MyString",
+#       :price_for_room => 1,
+#       :address => "MyString"
+#     ))
+#   end
 
-  it "renders the edit hotel form" do
-    render
+#   it "renders the edit hotel form" do
+#     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => hotels_path(@hotel), :method => "post" do
-      assert_select "input#hotel_title", :name => "hotel[title]"
-      assert_select "input#hotel_breakfast_included", :name => "hotel[breakfast_included]"
-      assert_select "textarea#hotel_room_description", :name => "hotel[room_description]"
-      assert_select "input#hotel_photo", :name => "hotel[photo]"
-      assert_select "input#hotel_price_for_room", :name => "hotel[price_for_room]"
-      assert_select "input#hotel_address", :name => "hotel[address]"
-    end
-  end
-end
+#     # Run the generator again with the --webrat flag if you want to use webrat matchers
+#     assert_select "form", :action => hotels_path(@hotel), :method => "post" do
+#       assert_select "input#hotel_title", :name => "hotel[title]"
+#       assert_select "input#hotel_breakfast_included", :name => "hotel[breakfast_included]"
+#       assert_select "textarea#hotel_room_description", :name => "hotel[room_description]"
+#       assert_select "input#hotel_photo", :name => "hotel[photo]"
+#       assert_select "input#hotel_price_for_room", :name => "hotel[price_for_room]"
+#       assert_select "input#hotel_address", :name => "hotel[address]"
+#     end
+#   end
+# end
