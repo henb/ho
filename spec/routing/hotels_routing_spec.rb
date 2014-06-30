bca93d110f099f856d6867f6678f276e465fddee
henb <henb2015@gmail.com>
2014-06-30 11:30:25 +0300

    Review tests to routing for rates

--- spec/routing/hotels_routing_spec.rb
+++ spec/routing/hotels_routing_spec.rb
@@ -3,33 +3,42 @@ require "spec_helper"
 describe HotelsController do
   describe "routing" do
 
+    it "routes to root" do
+      get("/").should be_routable
+    end
+
     it "routes to #index" do
-      get("/hotels").should route_to("hotels#index")
+      get("/hotels").should be_routable
     end
 
     it "routes to #new" do
-      get("/hotels/new").should route_to("hotels#new")
+      get("/hotels/new").should be_routable
     end
 
     it "routes to #show" do
-      get("/hotels/1").should route_to("hotels#show", :id => "1")
+      get("/hotels/1").should be_routable
     end
 
     it "routes to #edit" do
-      get("/hotels/1/edit").should route_to("hotels#edit", :id => "1")
+      get("/hotels/1/edit").should be_routable
     end
 
     it "routes to #create" do
-      post("/hotels").should route_to("hotels#create")
+      post("/hotels").should be_routable
     end
 
     it "routes to #update" do
-      put("/hotels/1").should route_to("hotels#update", :id => "1")
+      put("/hotels/1").should be_routable
     end
 
     it "routes to #destroy" do
-      delete("/hotels/1").should route_to("hotels#destroy", :id => "1")
+      delete("/hotels/1").should be_routable
+    end
+
+    it "routes to #top" do
+      delete("/hotels/top").should be_routable
     end
 
   end
+
 end
